64297ef42dd29a6e0aad0600dc313bd1cb656d83
David González <david_gonzalez_97@hotmail.com>
2020-07-28 18:31:39 -0500

    Working

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.set('port', process.env.PORT || 5000);
+
+app.get("/", (req,res) =>{
+    res.send("You are on the homepage");
+})
+
+app.post("/post", (req,res) =>{
+    res.send(`Welcome ${req.body.user}`);
+})
+
+app.delete("/delete", (req,res) =>{
+    let taskId = req.body.taskId;
+    res.send("{delete : true}")
+})
+
+app.put("/put/:id", (req,res) =>{
+    let {id} = req.params;
+    res.send(`Task ${id} has been updated`);
+})
+
+
+app.listen(app.get('port'), () =>{
+    console.log(`Server on port ${app.get('port')}`);
+})
